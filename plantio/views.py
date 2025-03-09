cd561a933d158a441d3945c5eba80d3f653e1f96
kelson-pereira <kelson.op@gmail.com>
2025-03-08 21:59:01 -0300

    fix: favicon path

--- plantio/views.py
+++ plantio/views.py
@@ -3,7 +3,5 @@ from django.views.generic.base import RedirectView
 
 # Crie suas visualizações aqui.
 
-favicon = RedirectView.as_view(url='/static/imagens/favicon.ico', permanent=True)
-
 def home(request):
     return render(request, 'home.html')
--- univesp_pi_3/urls.py
+++ univesp_pi_3/urls.py
@@ -1,25 +1,26 @@
 """
-URL configuration for univesp_pi_3 project.
+Configurações de URL para projeto univesp_pi_3.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+A lista `urlpatterns` roteia URLs para visualizações. Para mais informações consulte:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Exemplos:
+Função views
+    1. Adicione a importação:  from my_app import views
+    2. Adicione a URL em urlpatterns:  path('', views.home, name='home')
+Views baseada em uma classe:
+    1. Adicione a importação:  from other_app.views import Home
+    2. Adicione a URL em urlpatterns:  path('', Home.as_view(), name='home')
+Incluindo outra configuração de URL:
+    1. Importe a função include(): from django.urls import include, path
+    2. Adicione a URL em urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
+from django.views.generic import RedirectView
 from plantio import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    re_path(r'^favicon\.ico$', views.favicon)
+    path('favicon.ico', RedirectView.as_view(url='/static/imagens/favicon.ico')),
 ]
