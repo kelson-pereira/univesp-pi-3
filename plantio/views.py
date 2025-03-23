ec6da11314deff3c2aecca87612ed2bff8fb8687
João Parra <j-r-p@outlook.com>
2025-03-23 17:41:03 -0300

    feat: led toggle

--- plantio/views.py
+++ plantio/views.py
@@ -11,3 +11,13 @@ def home(request):
 def led_status(request):
     led = Led.objects.first()
     return JsonResponse({'status': led.status})
+
+def toggle_led(request):
+    led, _ = Led.objects.get_or_create(id=1)
+    led.status = not led.status
+    led.save()
+    return JsonResponse({"led": led.status})
+
+def led_control_view(request):
+    led = Led.objects.first()
+    return render(request, "led_control.html", {"status": led.status if led else False})
