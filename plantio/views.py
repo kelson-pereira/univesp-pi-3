de2f3a2ac1b8fba38d3deb1245e9181c6c510ce4
João Parra <j-r-p@outlook.com>
2025-03-23 16:27:32 -0300

    feat: led/status

--- plantio/views.py
+++ plantio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import RedirectView
+from django.http import JsonResponse
+from .models import Led
 
 # Crie suas visualizações aqui.
 
 def home(request):
     return render(request, 'home.html')
+
+def led_status(request):
+    led = Led.objects.first()
+    return JsonResponse({'status': led.status})
