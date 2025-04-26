720c48126f3094d155f82a1aab4fbeb0cb54db33
kelson-pereira <kelson.op@gmail.com>
2025-04-26 14:45:16 -0300

    feat: level sensor

--- plantio/signals.py
+++ plantio/signals.py
@@ -6,6 +6,7 @@ def create_sensor_type(sender, **kwargs):
         {"name": "tmpA", "description": "Temperatura ambiente", "unit": "°C", "min_value": 20, "max_value": 32},
         {"name": "umdA", "description": "Umidade relativa do ar", "unit": "%", "min_value": 60, "max_value": 70},
         {"name": "tmpS", "description": "Temperatura da solução nutritiva", "unit": "°C", "min_value": 20, "max_value": 28},
+        {"name": "levS", "description": "Nível da solução nutritiva", "unit": "", "min_value": 0.5, "max_value": 1.5},
     ]
     for sensor in sensor_types:
         SensorType.objects.get_or_create(
